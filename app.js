2c53635c84aa8717524f91c8d5b5bfedeca7237b
Jesus Campos <jsuscrdz@hotmail.com>
2022-04-05 19:42:04 -0500

    Mi primer commit

---
+++ app.js
@@ -0,0 +1,13 @@
+/* let texto = prompt("Ingrese un texto");
+
+ */
+
+let texto = "Tkequisde";
+
+for(let pos in texto) {
+    if(pos.indexOf== 'a' | 'e' | 'i' | 'o' | 'u') {
+        return pos;
+    }else {
+        console.log("E");
+    }
+}
